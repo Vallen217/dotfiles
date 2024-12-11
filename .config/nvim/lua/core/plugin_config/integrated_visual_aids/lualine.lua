eec2178aac665a54ad08caa339caceba95f62d42
Vallen217 <EidolonVallen@gmail.com>
2024-12-11 01:04:27 -0600

    [nvim] Update lualine theme

--- .config/nvim/lua/core/plugin_config/integrated_visual_aids/lualine.lua
+++ .config/nvim/lua/core/plugin_config/integrated_visual_aids/lualine.lua
@@ -1,62 +1,43 @@
--- Bubbles config for lualine
--- Author: lokesh-krishna
--- MIT license, see LICENSE for more details.
-
--- stylua: ignore
-local colors = {
-	blue   = '#87bff7',
-	cyan   = '#9be9f8',
-	black  = '#12121f',
-	white  = '#d4d7ff',
-	red    = '#bd4277',
-	violet = '#9486e4',
-	grey   = '#29293f',
-}
-
-local bubbles_theme = {
-	normal = {
-		a = { fg = colors.black, bg = colors.violet },
-		b = { fg = colors.white, bg = colors.grey },
-		c = { fg = colors.black, bg = colors.black },
-	},
-
-	insert = { a = { fg = colors.black, bg = colors.blue } },
-	visual = { a = { fg = colors.black, bg = colors.cyan } },
-	replace = { a = { fg = colors.black, bg = colors.red } },
-
-	inactive = {
-		a = { fg = colors.white, bg = colors.black },
-		b = { fg = colors.white, bg = colors.black },
-		c = { fg = colors.black, bg = colors.black },
-	},
-}
-
-require("lualine").setup({
+require('lualine').setup {
 	options = {
-		theme = bubbles_theme,
-		component_separators = "|",
-		section_separators = { left = "", right = "" },
+		icons_enabled = true,
+		theme = 'eidolon',
+		component_separators = { left = '|', right = '|' },
+		section_separators = { left = '', right = '' },
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		always_show_tabline = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 100,
+			tabline = 100,
+			winbar = 100,
+		}
 	},
 	sections = {
-		lualine_a = {
-			{ "mode", separator = { left = "" }, right_padding = 2 },
-		},
-		lualine_b = { "filename", "branch" },
-		lualine_c = { "fileformat" },
-		lualine_x = {},
-		lualine_y = { "filetype", "progress" },
+		lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+		lualine_b = { 'branch', 'diff', 'diagnostics' },
+		lualine_c = { 'filename' },
+		lualine_x = { 'encoding', 'fileformat', 'filetype' },
+		lualine_y = { 'progress' },
 		lualine_z = {
-			{ "location", separator = { right = "" }, left_padding = 2 },
+			{ 'location', separator = { right = '' }, left_padding = 2 },
 		},
 	},
 	inactive_sections = {
-		lualine_a = { "filename" },
+		lualine_a = {},
 		lualine_b = {},
-		lualine_c = {},
-		lualine_x = {},
+		lualine_c = { 'filename' },
+		lualine_x = { 'location' },
 		lualine_y = {},
-		lualine_z = { "location" },
+		lualine_z = {}
 	},
 	tabline = {},
-	extensions = {},
-})
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {}
+}
