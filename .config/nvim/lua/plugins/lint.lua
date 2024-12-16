4e0465249cb61005f3e74508a389bee2711d1590
Vallen217 <EidolonVallen@gmail.com>
2024-12-16 01:43:18 -0600

    [nvim] Added linter support

---
+++ .config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,29 @@
+return {
+	"mfussenegger/nvim-lint",
+	config = function()
+		require("lint").linters_by_ft = {
+			c = { "cpplint" },
+			cpp = { "cpplint" },
+			sh = { "shellcheck" },
+			bash = { "bash" },
+			rust = { "clippy" },
+			python = { "pylint" },
+			lua = { "luacheck" },
+			sql = { "sqlfluff" },
+			markdown = { "Vale" },
+			html = { "htmlhint" },
+			css = { "stylelint" },
+			scss = { "stylelint-scss" },
+			yaml = { "spectral" },
+			json = { "spectral" },
+		}
+
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			callback = function()
+				local function try_lint(lint)
+					require("lint").try_lint(lint)
+				end
+			end,
+		})
+	end,
+}
